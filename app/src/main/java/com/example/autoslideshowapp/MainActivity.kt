273b45a379d264c53efaa1988de7d010865efbd5
yuyaprograming <ienjoyschoollife@gmail.com>
2020-07-18 23:00:00 +0900

    override fun onStop() {         super.onStop()         if (mCursor != null)             mCursor!!.close()
                を下のcloseを消して記入。

--- app/src/main/java/com/example/autoslideshowapp/MainActivity.kt
+++ app/src/main/java/com/example/autoslideshowapp/MainActivity.kt
@@ -37,6 +37,12 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    override fun onStop() {
+        super.onStop()
+        if (mCursor != null)
+            mCursor!!.close()
+    }
+
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
         when (requestCode) {
             PERMISSIONS_REQUEST_CODE ->
@@ -49,7 +55,7 @@ class MainActivity : AppCompatActivity() {
     private fun getContentsInfo() {
         // 画像の情報を取得する
         val resolver = contentResolver
-        val cursor = resolver.query(
+            mCursor = resolver.query(
             MediaStore.Images.Media.EXTERNAL_CONTENT_URI, // データの種類
             null, // 項目(null = 全項目)
             null, // フィルタ条件(null = フィルタなし)
@@ -60,16 +66,15 @@ class MainActivity : AppCompatActivity() {
         button1.setOnClickListener {
 
 
-            if (cursor!!.moveToFirst()) {
+            if (mCursor!!.moveToFirst()) {
                 // indexからIDを取得し、そのIDから画像のURIを取得する
-                val fieldIndex = cursor.getColumnIndex(MediaStore.Images.Media._ID)
-                val id = cursor.getLong(fieldIndex)
+                val fieldIndex = mCursor!!.getColumnIndex(MediaStore.Images.Media._ID)
+                val id = mCursor!!.getLong(fieldIndex)
                 val imageUri =
                     ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id)
 
                 imageView.setImageURI(imageUri)
             }
-            cursor.close()
         }
     }
 
