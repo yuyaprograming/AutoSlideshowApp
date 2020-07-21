fa53872fe594a21b1eef10fce66efbc5e88a2ff0
yuyaprograming <ienjoyschoollife@gmail.com>
2020-07-21 19:28:11 +0900

    再生ボタンで画像を再生できるように

--- app/src/main/java/com/example/autoslideshowapp/MainActivity.kt
+++ app/src/main/java/com/example/autoslideshowapp/MainActivity.kt
@@ -116,22 +116,30 @@ class MainActivity : AppCompatActivity() {
                         mHandler.post {
                             if (mCursor!!.moveToNext()) {
                                 // indexからIDを取得し、そのIDから画像のURIを取得する
-                                val fieldIndex = mCursor!!.getColumnIndex(MediaStore.Images.Media._ID)
+                                val fieldIndex =
+                                    mCursor!!.getColumnIndex(MediaStore.Images.Media._ID)
                                 val id = mCursor!!.getLong(fieldIndex)
                                 val imageUri =
-                                    ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id)
+                                    ContentUris.withAppendedId(
+                                        MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                                        id
+                                    )
 
                                 imageView.setImageURI(imageUri)
                             } else {
                                 mCursor!!.moveToFirst()
-                                val fieldIndex = mCursor!!.getColumnIndex(MediaStore.Images.Media._ID)
+                                val fieldIndex =
+                                    mCursor!!.getColumnIndex(MediaStore.Images.Media._ID)
                                 val id = mCursor!!.getLong(fieldIndex)
                                 val imageUri =
-                                    ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id)
+                                    ContentUris.withAppendedId(
+                                        MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                                        id
+                                    )
 
                                 imageView.setImageURI(imageUri)
                             }
-
+                        }
                     }
                 }, 2000, 2000) // 最初に始動させるまで 2000ミリ秒、ループの間隔を 2000ミリ秒 に設定
             }
